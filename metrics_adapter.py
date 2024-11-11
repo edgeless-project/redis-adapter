04e668def910842276334194afed2b7fb5705323
pantoni <p.antoniou@aegisresearch.eu>
2024-11-11 18:52:33 +0200

    fixed version

--- metrics_adapter.py
+++ metrics_adapter.py
@@ -13,27 +13,32 @@ redis_client = redis.Redis(host='127.0.0.1', port=6379, db=0)
 
 # Connect to Elasticsearch
 es = Elasticsearch(
-    ['elasticseach_url:9200'],
+    ['es_url'],
     basic_auth=('test', 'test'),
     verify_certs=False,
     ssl_show_warn=False
 )
 
+#current support indexes are: node, provider, dependency, instance, function, performance, workflow
 
+
+index_counter = 0
+#index Redis entries into Elasticsearch
 def index_to_elasticsearch(key, value):
     global index_counter
     # Print the Redis contents
     key_type = redis_client.type(key).decode()
     print(f"Key: {key.decode()}, Type: {key_type}")
+    # print(f"Value: {value.decode()}")
     key_parts = key.decode().split(':')
     index = key_parts[0]  # index type: node, provider, dependency, instance, function, performance, workflow
     timestamp = datetime.now().isoformat()
 
     # Default doc to None
     doc = None
-
+    # handle the case where value is a float (for lists type keys)
     if key_type == "string":
-        # Handle the case where the value is a string (JSON or float)
+    # Handle the case where the value is a string (JSON or float)
         try:
             doc = json.loads(value.decode())
         except json.JSONDecodeError:
@@ -51,35 +56,37 @@ def index_to_elasticsearch(key, value):
             {"value": float(item.decode().split(',')[0]), "timestamp": float(item.decode().split(',')[1])}
             for item in values
         ]
-    
+    # index based on the index type
     if index == "function":
         function_id = key_parts[1]
         
+        # Check if key is for 'average' or 'samples'
         if key_parts[2] == "average":
+            # Since it's an average, the value is a float
             doc = {'function_id': function_id, 'average': float(value.decode()), 'timestamp': timestamp}
         
         elif key_parts[2] == "samples":
+            # Handle 'samples' as a list of floats and timestamps
             doc = {
                 'function_id': function_id,
-                'samples': doc,
+                'samples': doc,  # the list we created from the 'lrange'
                 'timestamp': timestamp
             }
-        index_name = "tid_function"
+        index_name = "function"
 
     elif index == "node":
-        node_metric_type = key_parts[1]
+        node_metric_type = key_parts[1]  # "capabilities" or "health"
         node_id = key_parts[2]
         doc['timestamp'] = timestamp
         doc['node_id'] = node_id
 
         if node_metric_type == "capabilities":
-            index_name = "tid_node_capabilities"
+            index_name = "node_capabilities"
         elif node_metric_type == "health":
-            index_name = "tid_node_health"
+            index_name = "node_health"
         else:
             print(f"Unrecognized node metric type for key: {key.decode()}")
             return
-
     elif index == "provider":
         doc['timestamp'] = timestamp
         node_id = doc.get('node_id')
@@ -87,18 +94,18 @@ def index_to_elasticsearch(key, value):
             print(f"Skipping key: {key} due to missing node_id in provider entry")
             return
         doc['node_id'] = node_id
-        index_name = "tid_provider"
+        index_name = "provider"
     
     elif index == "dependency":
         doc['timestamp'] = timestamp
         doc['dependency_id'] = key_parts[1]
-        index_name = "tid_dependency"
+        index_name = "dependency"
         
     elif index == "instance":
         doc['timestamp'] = timestamp
         instance_id = key_parts[1]
         doc['instance_id'] = instance_id
-        
+        # Normalize Resource field if it exists
         if 'Resource' in doc:
             normalized_resource = []
             for item in doc['Resource']:
@@ -108,6 +115,7 @@ def index_to_elasticsearch(key, value):
                     normalized_resource.append({"InstanceId": item})
                 doc['Resource'] = normalized_resource
         
+        # Normalize Function field if it exists
         if 'Function' in doc:
             normalized_function = []
             for item in doc['Function']:
@@ -117,36 +125,43 @@ def index_to_elasticsearch(key, value):
                     normalized_function.append({"InstanceId": item})
                 doc['Function'] = normalized_function
         
-        index_name = "tid_instance"
+        index_name = "instance"
 
     elif index == "performance":
         function_id = key_parts[2]
+        # Handle performance data (list of execution times and timestamps)
         doc = {
             'function_id': function_id,
-            'execution_times': doc,
+            'execution_times': doc,  # List of parsed performance data
             'timestamp': timestamp
         }
-        index_name = "tid_performance"
+        index_name = "performance"
     
     elif index == "workflow":
+        #convert to dict if not
         if not isinstance(doc, dict):
             doc = {}
-        workflow_metric_type = key_parts[1]
+        workflow_metric_type = key_parts[1]  # e.g., vector_mul_wf_chain
         doc['workflow_type'] = workflow_metric_type
 
         if key_parts[2] == "average":
+            # Handle average value
             doc['average'] = float(value.decode())
+            
 
         elif key_parts[2] == "samples":
+            # Handle workflow samples
             samples = value.decode().split()
             doc['samples'] = [
                 {"value": float(sample.split(',')[0]), "timestamp": float(sample.split(',')[1])}
                 for sample in samples
             ]
-        index_name = "tid_workflow"
+        index_name = "workflow"
     else:
         print(f"Skipping key: {key.decode()} due to unrecognized index")
         return
+    
+    # print(f"Indexing into {index_name}: {json.dumps(doc, indent=2)}")
     
     # forward into Elasticsearch
     try:
@@ -154,5 +169,58 @@ def index_to_elasticsearch(key, value):
         res = es.index(index=index_name, body=doc)
         print(f"Indexed document: {index_counter})  {res['result']}")
 
+
+        # print(f"Total indexed documents: {index_counter}")
     except Exception as e:
         print(f"Error indexing document: {e}")
+
+# on the start of the script go to redis and fetch all existing entries
+def index_existing_entries():
+    cursor = 0
+    while True:
+        cursor, keys = redis_client.scan(cursor=cursor)
+        for key in keys:
+            key_type = redis_client.type(key).decode()
+
+            # Handle only string keys (type = "string")
+            if key_type == "string":
+                value = redis_client.get(key)
+                if value:
+                    index_to_elasticsearch(key, value)
+            elif key_type == "list":
+                values = redis_client.lrange(key, 0, -1)
+                if values:
+                    for value in values:
+                        index_to_elasticsearch(key, value)
+                # print(f"Skipping key: {key.decode()} due to incompatible type: {key_type}")
+            else:
+                print(f"Skipping key: {key.decode()} due to incompatible type: {key_type}")
+        
+        if cursor == 0:
+            break
+
+# keep listening on redis channel for new entries
+def listen_for_new_entries(redis_channel):
+    pubsub = redis_client.pubsub()
+    pubsub.psubscribe('__key*__:*')
+
+    print(f"Subscribed to Redis channel: {redis_channel}")
+
+    for message in pubsub.listen():
+        if message['type'] == 'pmessage':
+            key = message['data']
+            value = redis_client.get(key)
+            
+            if value:
+                print(f"received message for key: {key} {value}")
+            else:
+                print(f"Received message for key: {key}, but no value found in Redis")
+
+
+# Index all existing entries on script startup
+index_existing_entries()
+
+# Subscribe to listen for new entries
+if __name__ == "__main__":
+    redis_channel = '__key*__:*'  # Specify the Redis channel to subscribe to
+    listen_for_new_entries(redis_channel)
